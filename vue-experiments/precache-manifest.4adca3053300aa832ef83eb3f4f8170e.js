d9363e03eafa5a80a6cd25bde91492fd404af71b
Luiz Henrique Nascimento dos Anjos <luiz.anjos@contabilizei.com.br>
2019-09-28 18:10:41 -0300

    deploy vue-experiments

---
+++ vue-experiments/precache-manifest.4adca3053300aa832ef83eb3f4f8170e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "e99dd96ee8be99f17962",
+    "url": "/js/chunk-vendors.a241406e.js"
+  },
+  {
+    "revision": "861a3f2f7fe659db15b9",
+    "url": "/js/app.6750d44d.js"
+  },
+  {
+    "revision": "65e24c1aec90f5a160e04d5d73871441",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "861a3f2f7fe659db15b9",
+    "url": "/css/app.716210bc.css"
+  }
+];
